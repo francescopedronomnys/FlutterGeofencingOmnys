c9aaf6f1bdc34646d56df5224978cce2f7c4fccd
Francesco Pedron <francesco.pedron@omnys.com>
2023-05-31 11:10:04 +0200

    On android removed region change callback in didDetermineStateForRegion

--- android/src/main/kotlin/com/omnys/geofencing/flutter_geofencing_omnys/BeaconsClient.kt
+++ android/src/main/kotlin/com/omnys/geofencing/flutter_geofencing_omnys/BeaconsClient.kt
@@ -18,10 +18,7 @@ class BeaconsClient : MonitorNotifier {
             // Add parsing support for iBeacon
             // https://beaconlayout.wordpress.com/
             beaconManager.beaconParsers.clear()
-            beaconManager.beaconParsers.add(BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"))/*            beaconManager!!.beaconParsers.add(BeaconParser().setBeaconLayout("x,s:0-1=feaa,m:2-2=20,d:3-3,d:4-5,d:6-7,d:8-11,d:12-15"))
-                        beaconManager!!.beaconParsers.add(BeaconParser().setBeaconLayout("s:0-1=feaa,m:2-2=00,p:3-3:-41,i:4-13,i:14-19"))
-                        beaconManager!!.beaconParsers.add(BeaconParser().setBeaconLayout("s:0-1=feaa,m:2-2=10,p:3-3:-41,i:4-20v"))*/
-
+            beaconManager.beaconParsers.add(BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"))
             beaconManager.addMonitorNotifier(this)
         }
     }
@@ -49,10 +46,7 @@ class BeaconsClient : MonitorNotifier {
     }
 
     override fun didDetermineStateForRegion(state: Int, region: Region) {
-        when (state) {
-            MonitorNotifier.INSIDE -> didEnterRegion(region)
-            MonitorNotifier.OUTSIDE -> didExitRegion(region)
-        }
+
     }
 
     companion object {
